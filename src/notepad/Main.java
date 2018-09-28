8bc0bbf5a7a631ed55aeb0063309b7beb0c03f3f
poSobachii <grishanov@inbox.lv>
2018-09-29 02:10:21 +0300

    domashka

--- src/notepad/Main.java
+++ src/notepad/Main.java
@@ -14,9 +14,12 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Enter command:");
+            System.out.println("Enter command or type 'help' to see list of available commands ");
             String cmd = scan.next();
             switch (cmd) {
+                case "help":
+                    help();
+                    break;
                 case "create":
                     create();
                     break;
@@ -37,23 +40,39 @@ public class Main {
     private static void delete() {
         System.out.println("Type id number for delete");
         Integer del = scan.nextInt();
-        for (Person p : persons){
-            Integer ind = p.getId();
-            if (ind == del) {
-                persons.remove(ind);
+        for (Person a : persons) {
+            if (a.getId() == del) {
+                persons.remove(a);
                 break;
             }
-
         }
-                for (Person p : persons) {
+        for (Person p : persons) {
             System.out.println(p);
         }
     }
 
+//        for (Person p : persons){
+//            Integer ind = p.getId();
+//            if (ind == del) {
+//                persons.remove(ind);
+//                break;
+//            }
+//
+//        }
+
+
     private static void list() {
-        for (int i = 0; i < persons.size() ; i++) {
+        for (int i = 0; i < persons.size(); i++) {
             System.out.println(persons.get(i));
         }
+    }
+
+    private static void help() {
+        System.out.println("List of available commands: "
+                + '\n' + "create - to create new person"
+                + '\n' + "list - to see all persons"
+                + '\n' + "delete - to delete person"
+                + '\n' + "exit - to exit program");
     }
 
 //    private static void list() {
@@ -64,18 +83,33 @@ public class Main {
 
 
     private static void create() {
-        System.out.println("Enter Name: ");
-        String name = scan.next();
-        System.out.println("Enter Surname: ");
-        String surn = scan.next();
-        System.out.println("Enter phone number: ");
-        String phon = scan.next();
 
+        Scanner in = new Scanner(System.in);
         Person pers = new Person();
 
+        System.out.println("Enter Name: ");
+        String name = in.nextLine();
         pers.setName(name);
+
+        System.out.println("Enter Surname: ");
+        String surn = in.nextLine();
         pers.setSurname(surn);
-        pers.setPhone(phon);
+
+        do {
+            System.out.println("Enter phone number: ");
+            String phon = in.nextLine();
+            if (phon.length() <= 5) {
+                System.out.println("number to small");
+            } else {
+                pers.setPhone(phon);
+                break;
+            }
+        } while (true);
+
+        System.out.println("Enter email: ");
+        String ema = in.nextLine();
+        pers.setEmail(ema);
+
         persons.add(pers);
 
         System.out.println(pers);
--- src/notepad/Person.java
+++ src/notepad/Person.java
@@ -6,42 +6,33 @@ public class Person {
     private String name;
     private String surname;
     private String phone;
+    private String email;
 
 
-    public Person(){
+    public Person() {
 
-        count ++;
+        count++;
         id = count;
 
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public String getSurname() {
-        return surname;
-    }
+    public String getSurname() { return surname; }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+    public void setSurname(String surname) { this.surname = surname; }
 
-    public String getPhone() {
-        return phone;
-    }
+    public String getPhone() { return phone; }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 
     @Override
     public String toString() {
@@ -49,6 +40,7 @@ public class Person {
                 ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
                 '}' + count;
     }
 }
